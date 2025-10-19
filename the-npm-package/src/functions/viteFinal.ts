943ce05564ac49cbc64f3a3e305fa5f565d2eeb0
jramke <jstrmk@gmail.com>
2025-10-19 20:19:37 +0200

    fix: ensure correct build targets that support top level await

--- the-npm-package/src/functions/viteFinal.ts
+++ the-npm-package/src/functions/viteFinal.ts
@@ -4,6 +4,11 @@ import { glob } from 'node:fs/promises';
 import { basename } from 'node:path';
 import { vite404Plugin } from './vite404Plugin.ts';
 
+const colorYellow = '\x1b[33m';
+const colorRed = '\x1b[31m';
+const colorReset = '\x1b[0m';
+const logPrefix = colorYellow + '@andersundsehr/storybook-typo3:' + colorReset;
+
 async function isFeatureEnabled(name: string, envs: Record<string, string>): Promise<boolean> {
   return ['true', '1'].includes(envs[name] || '0');
 }
@@ -20,13 +25,12 @@ export async function viteFinal(config: InlineConfig, options: Options): Promise
   }
   config = addAllowedHosts(config);
   config = addProxyPlugin(config, envs.STORYBOOK_TYPO3_ENDPOINT);
+  config = ensureTopLevelAwaitSupport(config);
   return config;
 }
 
 async function watchOnlyStoriesConfig(config: InlineConfig, options: Options): Promise<InlineConfig> {
-  const colorYellow = '\x1b[33m';
-  const colorReset = '\x1b[0m';
-  console.log(colorYellow + '@andersundsehr/storybook-typo3:' + colorReset + ' STORYBOOK_TYPO3_WATCH_ONLY_STORIES enabled, only watching stories files');
+  console.log(logPrefix + ' STORYBOOK_TYPO3_WATCH_ONLY_STORIES enabled, only watching stories files');
 
   const defaultGlobPattern = '/**/*.@(mdx|stories.@(mdx|js|jsx|mjs|ts|tsx))';
   const storiesGlob = await options.presets.apply('stories');
@@ -68,5 +72,35 @@ function addAllowedHosts(config: InlineConfig): InlineConfig {
     config.server.allowedHosts ??= [];
     config.server.allowedHosts.push('.ddev.site');
   }
+  return config;
+}
+
+function ensureTopLevelAwaitSupport(config: InlineConfig): InlineConfig {
+  const minTargetsForTopLevelAwait = ['es2022', 'edge89', 'firefox89', 'chrome89', 'safari15'];
+  const incompatibleVersions = ['es2020', 'es2021', 'chrome87', 'edge88', 'firefox78', 'safari14'];
+
+  config.build = config.build || {};
+
+  if (config.build.target) {
+    const targets = Array.isArray(config.build.target) ? config.build.target : [config.build.target];
+    
+    const incompatibleTargets = targets.filter((target) => {
+      return incompatibleVersions.includes(target);
+    });
+
+    if (incompatibleTargets.length > 0) {
+      console.warn(
+        colorRed + logPrefix + ' WARNING: Your build target contains environments that do not support top-level await: ' + 
+        incompatibleTargets.join(', ') + colorReset
+      );
+      console.warn(
+        logPrefix + ' To support top-level await, consider using: ' +
+        minTargetsForTopLevelAwait.join(', ')
+      );
+    }
+  } else {
+    config.build.target = minTargetsForTopLevelAwait;
+  }
+
   return config;
 }
